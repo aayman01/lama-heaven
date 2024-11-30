5fe1eefa1cbb0e0a37d6f834db01980b385c7531
chandro4D <chandroshakhor7@gmail.com>
2024-11-30 22:28:36 +0600

    update contact page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -99,21 +99,37 @@ const about = () => {
                 <h1 className="text-5xl text-blue-900  font-bold">Our Core Values</h1>
                 <div className="flex justify-center pt-14">
                     <div className="w-[350px] mr-5 h-[200px] text-blue-900 bg-white shadow-xl rounded-lg shadow-slate-200">
-                        <h1 className="pt-16 text-2xl font-semibold">Integrity</h1>
+                        <div className="w-11 h-11 ml-[150px] mt-10 ">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                            ><path fill="#004080"
+                                d="M256 0c4.6 0 9.2 1 13.4 2.9L457.7 82.8c22 9.3 38.4 31 38.3 57.2c-.5 99.2-41.3 280.7-213.6 363.2c-16.7 8-36.1 8-52.8 0C57.3 420.7 16.5 239.2 16 140c-.1-26.2 16.3-47.9 38.3-57.2L242.7 2.9C246.8 1 251.4 0 256 0zm0 66.8l0 378.1C394 378 431.1 230.1 432 141.4L256 66.8s0 0 0 0z" />
+                            </svg>
+                        </div>
+                        <h1 className="pt-5 text-2xl font-semibold">Integrity</h1>
                         <p className="text-lg">We act with  in all that we do.</p>
                     </div>
                     <div className="w-[350px] mr-5 h-[200px] text-blue-900 bg-white shadow-xl rounded-lg shadow-slate-200">
-                        <h1 className="pt-16 text-2xl font-semibold">Trust</h1>
+                        <div className="w-11 h-11 ml-[150px] mt-10 ">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#004080" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" /></svg>
+                        </div>
+                        <h1 className="pt-5 text-2xl font-semibold">Trust</h1>
                         <p className="text-lg">We act with  in all that we do.</p>
                     </div>
                     <div className="w-[350px] h-[200px] text-blue-900 bg-white shadow-xl rounded-lg shadow-slate-200">
-                        <h1 className="pt-16 text-2xl font-semibold">Honesty</h1>
+                        <div className="w-11 h-11 ml-[150px] mt-10 ">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#004080" d="M448 256A192 192 0 1 0 64 256a192 192 0 1 0 384 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm0-224a144 144 0 1 1 0 288 144 144 0 1 1 0-288zM224 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" /></svg>
+                        </div>
+                        <h1 className="pt-5 text-2xl font-semibold">Honesty</h1>
                         <p className="text-lg">We act with  in all that we do.</p>
                     </div>
                 </div>
             </div>
 
-            <div className="text-center w-full h-[350px] bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+            <div className="text-center w-full h-[400px] bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+                <div className="w-16 h-16 ml-[725px] pt-16 ">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#c0c0c0" d="M96 0C43 0 0 43 0 96L0 416c0 53 43 96 96 96l288 0 32 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l0-64c17.7 0 32-14.3 32-32l0-320c0-17.7-14.3-32-32-32L384 0 96 0zm0 384l256 0 0 64L96 448c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16zm16 48l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z" /></svg>
+                </div>
                 <h1 className="text-5xl pt-24 text-white font-bold">Our Story</h1>
                 <p className="text-2xl font-normal pt-5 text-white">Founded in 2024, UrbanDrive has quickly grown
                     to become a trusted name in <br /> car rentals, known for our dedication to customer service and high-quality <br /> vehicles.</p>
@@ -123,7 +139,15 @@ const about = () => {
                 <h1 className="text-5xl text-blue-900 font-bold">Get in Touch</h1>
                 <p className="text-2xl font-normal pt-5 text-neutral-600">Have questions? Reach out to us at .</p>
                 <button className="px-4 py-2 mt-7 w-32 h-16 bg-yellow-500 text-white font-bold rounded-lg shadow hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-400">
-                    Send
+                    <div className="flex">
+                        <div className="w-7 h-7 mr-3 ml-2  ">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+                        </div>
+                        <div>
+                            <h1 className="text-white">Send</h1>
+                        </div>
+                    </div>
+
                 </button>
 
             </div>
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Footer from "../../components/Footer";
 import Navbar from "../../components/shared/Navbar";
 const contact = () => {
-         
+
 
     return (
         <div>
@@ -19,6 +19,7 @@ const contact = () => {
                 >
                     <div className="absolute inset-0 bg-black bg-opacity-30"></div>
                     <div className="relative z-10 text-white p-6 sm:p-8 lg:p-10">
+
                         <h1 className="text-5xl sm:text-4xl lg:text-5xl font-extrabold mb-2">
                             Contact Us
                         </h1>
@@ -28,20 +29,32 @@ const contact = () => {
                     </div>
                 </div>
                 <div className="absolute top-[420px] left-0 right-0 z-20 flex justify-center space-x-6">
-                    <div className="rounded-lg text-center w-[260px] h-[150px] bg-white shadow-xl shadow-slate-500">
-                        <h1 className="mt-12 text-blue-900 text-xl font-semibold">Email us</h1>
+                    <div className="rounded-lg text-center w-[280px] h-[150px] bg-white shadow-xl shadow-slate-500">
+                        <div className="w-7 h-7 ml-[130px] pt-7 mb-12 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#004080" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+                        </div>
+                        <h1 className=" text-blue-900 text-xl font-semibold">Email us</h1>
                         <h1 className="text-base font-normal">info@lamaheaven.com</h1>
                     </div>
-                    <div className="rounded-lg text-center w-[260px] h-[150px] bg-white shadow-xl shadow-slate-500">
-                        <h1 className="mt-12 text-blue-900 text-xl font-semibold">Our address</h1>
+                    <div className="rounded-lg text-center w-[280px] h-[150px] bg-white shadow-xl shadow-slate-500">
+                        <div className="w-7 h-7 ml-[130px] pt-7 mb-12 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#004080" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+                        </div>
+                        <h1 className=" text-blue-900 text-xl font-semibold">Our address</h1>
                         <h1 className="text-base font-normal">Dhaka, Bangladesh</h1>
                     </div>
-                    <div className="rounded-lg text-center w-[260px] h-[150px] bg-white shadow-xl shadow-slate-500">
-                        <h1 className="mt-12 text-blue-900 text-xl font-semibold">Opening Hours</h1>
+                    <div className="rounded-lg text-center w-[280px] h-[150px] bg-white shadow-xl shadow-slate-500">
+                        <div className="w-7 h-7 ml-[130px] pt-7 mb-12 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#004080" d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
+                        </div>
+                        <h1 className=" text-blue-900 text-xl font-semibold">Opening Hours</h1>
                         <h1 className="text-base font-normal">Mon-Sun: 8 AM - 7 PM</h1>
                     </div>
-                    <div className="rounded-lg text-center w-[260px] h-[150px] bg-white shadow-xl shadow-slate-500">
-                        <h1 className="mt-12 text-blue-900 text-xl font-semibold">Contact Phone</h1>
+                    <div className="rounded-lg text-center w-[280px] h-[150px] bg-white shadow-xl shadow-slate-500">
+                        <div className="w-7 h-7 ml-[130px] pt-7 mb-12 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#004080" d="M280 0C408.1 0 512 103.9 512 232c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-101.6-82.4-184-184-184c-13.3 0-24-10.7-24-24s10.7-24 24-24zm8 192a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm-32-72c0-13.3 10.7-24 24-24c75.1 0 136 60.9 136 136c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-48.6-39.4-88-88-88c-13.3 0-24-10.7-24-24zM117.5 1.4c19.4-5.3 39.7 4.6 47.4 23.2l40 96c6.8 16.3 2.1 35.2-11.6 46.3L144 207.3c33.3 70.4 90.3 127.4 160.7 160.7L345 318.7c11.2-13.7 30-18.4 46.3-11.6l96 40c18.6 7.7 28.5 28 23.2 47.4l-24 88C481.8 499.9 466 512 448 512C200.6 512 0 311.4 0 64C0 46 12.1 30.2 29.5 25.4l88-24z"/></svg>
+                        </div>
+                        <h1 className=" text-blue-900 text-xl font-semibold">Contact Phone</h1>
                         <h1 className="text-base font-normal">+880 1234 567890</h1>
                     </div>
                 </div>
@@ -50,17 +63,17 @@ const contact = () => {
                 <div>
                     <h1 className="text-4xl font-bold pt-40 text-center">Get In Touch</h1>
                 </div>
-                <div className="mt-16 flex">
-                    <div className="pt-10 pl-10 w-[615px] h-[500px] bg-white shadow-lg shadow-slate-400 rounded-lg ml-10">
+                <div className="mt-16 ml-[250px] flex">
+                    <div className="pt-10 pl-10 w-[870px] h-[500px] bg-white shadow-lg shadow-slate-400 rounded-lg ml-10">
                         <div className="flex mb-5 ">
-                            <div className="mr-5  items-center gap-2 border border-gray-300 rounded-lg px-7 py-2">
+                            <div className="mr-5  items-center gap-2 border border-gray-300 rounded-lg px-24 py-2">
                                 <input
                                     type="text"
                                     className="grow p-2 border-none outline-none"
                                     placeholder="Your Name*"
                                 />
                             </div>
-                            <div className=" items-center gap-2 border border-gray-300 rounded-lg px-7 py-2">
+                            <div className=" items-center gap-2 border border-gray-300 rounded-lg px-24 py-2">
                                 <input
                                     type="email"
                                     className="grow p-2 border-none outline-none"
@@ -69,14 +82,14 @@ const contact = () => {
                             </div>
                         </div>
                         <div className="flex mb-5">
-                            <div className="mr-5 items-center gap-2 border border-gray-300 rounded-lg px-7 py-2">
+                            <div className="mr-5 items-center gap-2 border border-gray-300 rounded-lg px-24 py-2">
                                 <input
                                     type="number"
                                     className="grow p-2 border-none outline-none"
                                     placeholder="Your Phone Number*"
                                 />
                             </div>
-                            <div className=" items-center gap-2 border border-gray-300 rounded-lg px-7 py-2">
+                            <div className=" items-center gap-2 border border-gray-300 rounded-lg px-24 py-2">
                                 <input
                                     type="text"
                                     className="grow p-2 border-none outline-none"
@@ -84,20 +97,20 @@ const contact = () => {
                                 />
                             </div>
                         </div>
-                        <div className=" items-center gap-2 border border-gray-300 rounded-lg w-[530px]  py-16">
+                        <div className=" items-center gap-2 border border-gray-300 rounded-lg w-[800px]  py-16">
                             <input
                                 type="text"
                                 className="grow p-5 border-none outline-none"
                                 placeholder="Message*"
                             />
                         </div>
-                        <button className="px-4 py-2 mt-7 w-[530px] h-12 bg-gradient-to-r from-indigo-950 to-sky-600 text-white font-bold rounded-2xl shadow hover:bg-sky-500  focus:outline-none focus:ring-2 focus:ring-yellow-400">
+                        <button className="px-4 py-2 mt-7 w-[800px] h-12 bg-gradient-to-r from-indigo-950 to-sky-600 text-white font-bold rounded-2xl shadow hover:bg-sky-500  focus:outline-none focus:ring-2 focus:ring-yellow-400">
                             Submit
                         </button>
 
                     </div>
                     <div>
-                        
+
 
                     </div>
                 </div>
